9ea596c9107a26cab2bb6f580217f48e91b9ddb0
q4w3e2r1 <126683181+q4w3e2r1@users.noreply.github.com>
2023-04-10 19:40:08 +0500

    Персонаж двигается по экрану и может крутиться понаправлению курсора

---
+++ GameProject/Sourse/GamePlay/World.cs
@@ -0,0 +1,38 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace GameProject.Sourse.GamePlay
+{
+    public class World
+    {
+
+        public Hero hero;
+        public World()
+        {
+            hero = new Hero("2d.1\\NormalHero", new Vector2(300, 300), new Vector2(48, 48));
+        }
+
+        public virtual void Update()
+        {
+            hero.Update();
+        }
+
+        public virtual void Draw(Vector2 OFFSET)
+        {
+            hero.Draw(OFFSET);
+        }
+    }
+}
